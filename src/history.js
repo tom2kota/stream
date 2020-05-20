9b2ce0ef179c6be3b9606a4adbba0228a4b27804
Tom Kota <63372913+tom2kota@users.noreply.github.com>
2020-05-20 21:37:48 +0300

    Add files via upload

---
+++ src/history.js
@@ -0,0 +1,6 @@
+// use: require("history").createBrowserHistory` instead of: require("history/createBrowserHistory")
+// import createHistory from "history/createBrowserHistory";
+
+import {createBrowserHistory} from "history";
+
+export default createBrowserHistory()
